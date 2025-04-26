907378444326114b9158a03d24439f3a5abe0634
HillsRain-ultra <hieuvu28022003@gmail.com>
2025-04-26 16:24:30 +0700

    optimize management

--- src/main/java/dadn_SmartFarm/service/implement/DeviceService.java
+++ src/main/java/dadn_SmartFarm/service/implement/DeviceService.java
@@ -24,10 +24,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 
 @Service
